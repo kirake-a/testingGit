a21d73a342e9f0ca2dda72762cfa79f76b278f71
kirake-a <ruben_aalvarado@outlook.com>
2022-02-18 15:12:49 -0600

    Se agrego nueva funcionalidad a BExample

--- c/BExample.c
+++ c/BExample.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
 int main(){
+    int valor;
+
     printf("apostrofe(') \t \t");
     printf("comillas('' '') \t \t");
     printf("diagonal invertida (/) \n" );
     printf("*Ruben Enrique Alvarado Interian*");
 
+    printf("Digite un valor entero que le guste: ");
+    scanf("%d", &valor);
+
+    printf("El valor que usted proporciono es el: %d" valor);
     return 0;
 }
